55ba255f52658611ad31596ea1963ead8299d11a
KalebParra007 <desarrollo@academicos.cesde.edu.co>
2024-11-07 15:37:38 -0500

    Se subio el proyecto con nuevas funciones para hacer la verificacion de lo que se escribe en los inputs por medio de las expresiones Regulares, ademas de que se aprendio a como usar el import y el export

---
+++ controller/ControllerCredito.js
@@ -0,0 +1,37 @@
+// function Personas(nombre,edad,estatura,color){
+//     this.nombre=nombre
+//     this.edad=edad
+//     this.estatura=estatura
+//     this.color=color
+
+//     this.moverse=function(nombre)
+//     {console.log(this.nombre=nombre+" se ha movido");}
+
+//     this.expresarse=function()
+//     {console.log("Expresado");}
+// }
+// let personaUno=new Personas('Roberto',18,1.65,'Blanco');
+// let personaDos=new Personas('Carlos',18,1.90,'Blanco');
+// let personaTres=new Personas('Cristian',18,1.58,'Moreno');
+
+// console.log(personaUno, personaDos)
+
+// personaUno.moverse()
+// personaDos.expresarse()
+// personaTres.expresarse()
+
+
+// let personas = {
+//   nombre: "Juan",
+//   apellido: "Bolaños",
+//   edad: 19,
+//   moverse: function () {
+//     console.log("Se ha movido");
+//   },
+
+//   direcion: {
+//     calle: 23,
+//     carrera: 23,
+//     interior: 20,
+//   },
+// };
--- views/Public/js/script.js
+++ views/Public/js/script.js
@@ -1,80 +1,39 @@
-// function Personas(nombre,edad,estatura,color){
-//     this.nombre=nombre
-//     this.edad=edad
-//     this.estatura=estatura
-//     this.color=color
-
-//     this.moverse=function(nombre)
-//     {console.log(this.nombre=nombre+" se ha movido");}
-
-//     this.expresarse=function()
-//     {console.log("Expresado");}
-// }
-// let personaUno=new Personas('Roberto',18,1.65,'Blanco');
-// let personaDos=new Personas('Carlos',18,1.90,'Blanco');
-// let personaTres=new Personas('Cristian',18,1.58,'Moreno');
-
-// console.log(personaUno, personaDos)
-
-// personaUno.moverse()
-// personaDos.expresarse()
-// personaTres.expresarse()
-
-let personas = {
-  nombre: "Juan",
-  apellido: "Bolaños",
-  edad: 19,
-  moverse: function () {
-    console.log("Se ha movido");
-  },
-
-  direcion: {
-    calle: 23,
-    carrera: 23,
-    interior: 20,
-  },
-};
-
-let formularioLogin = document.getElementById("form-login");
-
-let formularioRegister = document.querySelector("#form-register");
-
+import { expresionesRegulares } from "../../../Helpers/expresiones.js";
 document
   .querySelector("#btnFormRegistro")
-  .addEventListener("click", function(){
+  .addEventListener("click", function () {
     document.querySelector("#form-login").style.display = "flex";
     document.querySelector("#form-register").style.display = "none";
   });
 
-document.querySelector("#btnFormLogin").addEventListener("click",function(){
+document.querySelector("#btnFormLogin").addEventListener("click", function () {
   document.querySelector("#form-login").style.display = "none";
   document.querySelector("#form-register").style.display = "flex";
 });
 
-let inputs =document.getElementsByTagName("input");
-for(let index=0; index< inputs.length; index++){
-    inputs[index].addEventListener("keyup", validarFormulario)
+let inputs = document.getElementsByTagName("input");
+for (let index = 0; index < inputs.length; index++) {
+  inputs[index].addEventListener("keyup", validarFormulario);
 }
 
-function validarFormulario(e){
-    switch(e.target.name){
-        case "login-usuario":
-            console.log("Este es el formulario de login Usuario");
-            break;
-        case "login-contraseña":
-            console.log("Este es el formulario de login contraseña");
-        case  "registerNombre":
-            console.log("Este es el formulario de registro usuario");
-            break;
-        case "register":
+function validarFormulario(e) {
+  // console.log(e);
+  switch (e.target.name) {
+    case "registerNombre":
+      if(expresionesRegulares.nombre.test(e.target.value)){
+        console.log("Pasó la validacion");
+      } else{console.log("El campo solo permite letras en mayúscula");
+      }
+      break;
+    case "registerUsuario":
+      if(expresionesRegulares.usuario.test(e.target.value)){
+        console.log("Pasó la validacion");        
+      }else{console.log("Solo letras minúsculas, entre 4 y 10 caracteres. ");
+      
+      }
 
-        default:
-            break;
 
-    }
+    default:
+      break;
+  }
 }
-
-
-// let array=['Kaval', true, function(){}, {nombre: 'Kaval'}, arrays=[]]
-
-// console.log(array)
