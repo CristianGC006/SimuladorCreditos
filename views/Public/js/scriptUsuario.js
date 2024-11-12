d8f7413201f699a7d79a9411d08e39aff145d259
Kaleb <programacionyoa@gmail.com>
2024-11-12 16:13:46 -0500

    Se usaron nuevos metodos en JS, se almacenaron datos en el array de usuarios y tambien combinamos el modelo VC Controller

---
+++ views/Public/js/scriptUsuario.js
@@ -0,0 +1,5 @@
+import {login, register} from '../../../controller/ControllerUsuario.js'
+
+
+document.getElementById("btn-registrar").addEventListener("click",register)
+document.getElementById("btn-login").addEventListener("click",login)
